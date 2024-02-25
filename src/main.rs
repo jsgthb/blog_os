b7007eedc86b088aa61d8a330fdbe401f1c0bc33
Jeff Schaack <jeff.schaack@gmail.com>
2024-02-25 20:52:38 +0100

    Implemented panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 #![no_std] // Disable standard library as it depends on underlying operating system
 
+use core::panic::PanicInfo;
+
+// Function is called on panic
+// Required as panic handler is typically defined in standard libary
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 fn main() {}
