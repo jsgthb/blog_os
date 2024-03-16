186fb28b15a62a5fc7141af9641b7d0f282cb894
jsgthb <jeff.schaack@gmail.com>
2024-03-16 19:13:32 +0100

    Added println macro to start and panic functions

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use core::panic::PanicInfo;
 // Function is called on panic
 // Required as panic handler is typically defined in standard libary
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -15,9 +16,6 @@ mod vga_buffer;
 // Operating system entry point
 #[no_mangle] // Disable name mangling so start function is not renamed on compilation
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("This is a test").unwrap();
-    write!(vga_buffer::WRITER.lock(), "The magic number is {}", 42).unwrap();
-    
+    println!("This is test number {}", 42);
     loop {}
 }
