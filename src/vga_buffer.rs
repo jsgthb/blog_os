881fc2a8196947442abae14bf104a19f6c3d38fd
jsgthb <jeff.schaack@gmail.com>
2024-03-16 18:12:32 +0100

    Added colour code struct

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -21,3 +21,14 @@ pub enum Color {
     Yellow = 14,
     White = 15,
 }
+
+// Struct containing full colour byte (i.e. foreground & background)
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
