46b259f78cd38815758c1e620e2b0205e9c1c049
jsgthb <jeff.schaack@gmail.com>
2024-03-16 18:40:15 +0100

    Replaced temporary vga buffer with module

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,12 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 // Operating system entry point
 #[no_mangle] // Disable name mangling so start function is not renamed on compilation
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
     
     loop {}
 }
