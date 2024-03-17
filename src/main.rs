23c6cbcf4dcd92111abd878a812fa09bd7621a91
jsgthb <jeff.schaack@gmail.com>
2024-03-17 16:32:27 +0100

    Added qemu exit function for test framework

--- src/main.rs
+++ src/main.rs
@@ -26,17 +26,37 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+// Custom test framework
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
+// Example test case
 #[test_case]
 fn trivial_assertion() {
     print!("Trivial assertion...");
     assert_eq!(1, 1);
     println!("[ok]")
+}
+
+// Qemu exit function for testing framework
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    // Write exit code to qemu exit device
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32)
+    }
 }
