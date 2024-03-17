7c7206ce680a59d43fa0904ce6b3b14991c0de3c
jsgthb <jeff.schaack@gmail.com>
2024-03-17 21:03:26 +0100

    Added serial print macros

--- src/serial.rs
+++ src/serial.rs
@@ -11,3 +11,26 @@ lazy_static! {
         Mutex::new(serial_port)
     };
 }
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    use core::fmt::Write;
+    SERIAL1.lock().write_fmt(args).expect("Printing to serial failed")
+}
+
+// Prints to host through serial interace
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*))
+    };
+}
+
+// Prints to host through serial interace including newline
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+        concat!($fmt, "\n"), $($arg)*));
+}
