bb3d15cd4909ee864870592228d474f19365eb41
jsgthb <jeff.schaack@gmail.com>
2024-03-24 18:21:45 +0100

    Boot integration test case

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,6 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use blog_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -16,4 +17,9 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output")
 }
