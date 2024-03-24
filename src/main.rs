de2783c5b85b48b0049c3fc1d009d5f8de94dbb1
jsgthb <jeff.schaack@gmail.com>
2024-03-24 18:43:58 +0100

    Added CPU breakpoint interrupt

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,14 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Custom kernel has started");
 
+    blog_os::init();
+
+    // Breakpoint test
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("Kernel is still working");
     loop {}
 }
